d739c83d9d8056581faa0222b605c3fdb31ace30
TrentonFunt <81596595+TrentonFunt@users.noreply.github.com>
2024-01-03 05:54:47 +0100

    Tasks Completed

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+first_w = word[:3]
+last_w = word[-2:]
+middle_w = word[1:-1]
+print(f"First 3 letters: {first_w}")
+print(f"Last 2 letters: {last_w}")
+print(f"Middle word: {middle_w}")
