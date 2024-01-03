c33aa433446293a1e14e5d74b4786afdbd802173
fasina.b <boluayofashina@gmail.com>
2024-01-03 02:52:05 +0100

    Tasks completed

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+first_w = word[:3]
+last_w = word[-2:]
+middle_w = word[1:-1]
+print(f"First 3 letters: {fw}")
+print(f"Last 2 letters: {lw}")
+print(f"Middle word: {mw}")
