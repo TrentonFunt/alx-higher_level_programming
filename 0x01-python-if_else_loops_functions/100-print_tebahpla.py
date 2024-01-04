7d54a3910e97b978ab7e059119a47466f59a7778
TrentonFunt <sphinixphase17@gmail.com>
2024-01-04 01:41:49 +0100

    Final

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 
 for c in range(ord('z'), ord('A') - 1, -1):
-    print("{:c}".format(c if (c - ord('A')) % 58 < 26 else c + 32), end="")
+    print("{:c}".format(c if (c - ord('A')) % 2 == 0 else c + 32), end="")
+
+print()
