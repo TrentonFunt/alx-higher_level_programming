6afe6081921ab8797738442aaa73e21413006f5d
TrentonFunt <sphinixphase17@gmail.com>
2024-01-04 01:16:25 +0100

    Correct syntax errors

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 for c in range(ord('z'), ord('A') - 1, -1):
-    print("{:c}".format(c if (c - ord('A')) % 4 == 0 else c + 32), end="")
+    print("{:c}".format(c if (c - ord('A')) % 4 < 2 else c + 32), end="")
