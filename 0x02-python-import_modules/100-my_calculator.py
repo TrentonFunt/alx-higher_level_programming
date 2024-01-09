3de612c5c31d917d805441ea94179596b3a8299b
TrentonFunt <sphinixphase17@gmail.com>
2024-01-09 19:33:58 +0100

    Fix pycodestyle validation

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,7 +2,9 @@
 import sys
 from calculator_1 import add, sub, mul, div
 
+
 def my_calculator():
+    # Check the number of arguments
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
@@ -26,7 +28,9 @@ def my_calculator():
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
+    # Print the result
     print("{} {} {} = {}".format(a, operator, b, result))
+
 
 if __name__ == "__main__":
     my_calculator()
--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-str = "#pythoniscool"; __import__('os').write(1, str.encode())
+__import__('os').write(1, "#pythoniscool\n".encode())
