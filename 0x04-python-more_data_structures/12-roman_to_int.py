7cc921ee9808e65dc0b1151781e7aaf886f7e613
TrentonFunt <sphinixphase17@gmail.com>
2024-01-11 01:19:06 +0100

    Finish Tasks 9 - 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                  'C': 100, 'D': 500, 'M': 1000}
+
+    total = 0
+    prev_value = 0
+
+    for char in roman_string:
+        if char in roman_dict:
+            current_value = roman_dict[char]
+            total += current_value
+
+            if prev_value < current_value:
+                total -= 2 * prev_value
+
+            prev_value = current_value
+        else:
+            return 0
+
+    return total
