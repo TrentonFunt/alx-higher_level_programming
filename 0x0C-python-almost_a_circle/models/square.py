f5ee367864bdd8b28f80e4ed48a55cd9b297a6e9
TrentonFunt <sphinixphase17@gmail.com>
2024-02-13 00:25:24 +0100

    Finish Square tasks

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Square module"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square class inherits from Rectangle"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialize Square instance"""
+
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """Getter for size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter for size"""
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """Assign attributes based on provided arguments"""
+
+        if args:
+            attrs = ["id", "size", "x", "y"]
+            for i, arg in enumerate(args):
+                setattr(self, attrs[i], arg)
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+    def __str__(self):
+        """Return string representation of Square instance"""
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width)
