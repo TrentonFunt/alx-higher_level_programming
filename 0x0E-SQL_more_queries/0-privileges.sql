393cf1bc9027a8446cb70d8f3d3f662842db9d2a
TrentonFunt <sphinixphase17@gmail.com>
2024-03-19 00:56:33 +0100

    Finish Task 1

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,17 +1,5 @@
--- List privileges of users user_0d_1 and user_0d_2
-SELECT 
-    user, 
-    host, 
-    db, 
-    select_priv, 
-    insert_priv, 
-    update_priv, 
-    delete_priv, 
-    create_priv, 
-    drop_priv, 
-    grant_priv 
-FROM 
-    mysql.user 
-WHERE 
-    user IN ('user_0d_1', 'user_0d_2') 
-    AND host = 'localhost';
+-- List privileges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- List privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
