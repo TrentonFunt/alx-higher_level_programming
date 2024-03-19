9375143effd80c79f38983e693eb01de4b963a0f
TrentonFunt <sphinixphase17@gmail.com>
2024-03-19 01:38:58 +0100

    Finish Task 11-13

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- List all genres and the number of shows linked to each
+SELECT tv_show_genres.genre_id AS genre,
+       COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_show_genres
+GROUP BY tv_show_genres.genre_id
+HAVING number_of_shows > 0
+ORDER BY number_of_shows DESC;
